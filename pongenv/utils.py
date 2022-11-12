e5fd334ec2a8005cbd445dd91cd3ca06d6fcef4c
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-11-12 02:22:25 +0100

    env utils

---
+++ pongenv/utils.py
@@ -0,0 +1,15 @@
+# from typing import Dict, List, Tuple
+from enum import IntEnum
+
+
+class PongEnvException(Exception):
+    pass
+
+
+class Action(IntEnum):
+    STAY = 0  # NOOP
+    MOVETOLEFT = 1
+    MOVETORIGHT = 2
+
+
+NUM_ACTIONS = len(Action)
