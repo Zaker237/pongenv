bc4a1da2a232fdbf8ed527275e51e24f801cb2cf
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-11-13 21:53:01 +0100

    implement get possible actions

--- ponggame/ponggame.py
+++ ponggame/ponggame.py
@@ -9,6 +9,7 @@ class PongGame(object):
     def __init__(
         self,
         num_players: int,
+        rng: np.random.RandomState,
         max_score: int = 10
     ) -> None:
         if num_players < 1 or num_players > 2:
@@ -16,13 +17,31 @@ class PongGame(object):
 
         self.finished = False
 
+        self.num_players = num_players
+
+        self.active_player = rng.choice(num_players)
+
     def sample_action(self, player: int) -> Action:
         actions = np.array(Action)[self.get_possible_actions()[player]]
 
         return self.rng.choice(actions)
+    
+    def get_possible_actions(self) -> np.ndarray:
+        actions = np.zeros((self.num_players, NUM_ACTIONS), bool)  # for every player which action is possible
+
+        return actions
 
     def get_scores(self) -> np.ndarray:
-        scores = np.zeros((self.num_players, 6), dtype=np.int16)
+        scores = np.zeros((self.num_players, 1), dtype=np.int16)
 
-    def play(self):
-        pass
+        return scores.sum(1)
+
+    def take_actions(self, actions: np.ndarray) -> int:
+        active_player = self.active_payer
+        possible_actions = self.get_possible_actions()
+        if not possible_actions[actions].all():
+            raise PongEnvException("Invalid Action chosed")
+        if not actions.any(1).all():
+            raise PongEnvException("Not all Agent chosed an action!")
+        if actions.sum() != self.num_players:
+            raise PongEnvException("An Agent chosed mor than one action!")
