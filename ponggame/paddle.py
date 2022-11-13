02fb90ff9ad55f8e47c16b5760ae0868d2e1e62b
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-11-13 22:15:35 +0100

    Paddle as state

--- ponggame/paddle.py
+++ ponggame/paddle.py
@@ -6,7 +6,7 @@ class Paddle(object):
     COLOR = WHITE
     VELOCITY = 4
 
-    def __init__(self, x, y, width, height):
+    def __init__(self, x: int, y: int, width: int, height: int) -> None:
         self.original_x = x
         self.original_y = y
         self.x = x
@@ -14,15 +14,23 @@ class Paddle(object):
         self.width = width
         self.height = height
 
-    def draw(self, win):
+    def draw(self, win) ->None:
         pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
-    def move(self, up=True):
+    def move(self, up=True) -> None:
         if up:
             self.y -= self.VELOCITY
         else:
             self.y += self.VELOCITY
 
-    def reset(self):
+    def reset(self) ->None:
         self.x = self.original_x
         self.y = self.original_y
+
+    def as_state(self) ->PADDLE_STATE:
+        return(
+            self.x,
+            self.y,
+            self.width,
+            self.height
+        )
