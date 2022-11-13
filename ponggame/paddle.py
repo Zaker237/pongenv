1a60363a18863cdc3827a8c3ff93582b273c90f2
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-11-13 22:02:07 +0100

    implement paddel

---
+++ ponggame/paddle.py
@@ -0,0 +1,27 @@
+import pygame
+from ponggame.utils import WHITE
+
+class Paddle(object):
+    COLOR = WHITE
+    VELOCITY = 4
+
+    def __init__(self, x, y, width, height):
+        self.original_x = x
+        self.original_y = y
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VELOCITY
+        else:
+            self.y += self.VELOCITY
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
