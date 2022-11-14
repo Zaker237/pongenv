147b6c33647fbcdcb0b34953782ec3a3a83671e8
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-11-14 08:54:53 +0100

    function to handel collision

--- ponggame/utils.py
+++ ponggame/utils.py
@@ -32,3 +32,31 @@ def handel_paddel_movement(keys, left_paddle: Paddle, right_paddle: Paddle):
         right_paddle.move(up=True)
     if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VELOCITY + right_paddle.height <= HEIGHT:
         right_paddle.move(up=False)
+
+
+def handel_collision(ball: Ball, left_paddle: Paddle, right_paddle: Paddle):
+    if ball.y + ball.raduis >= HEIGHT:
+        ball.y_velocity *= -1
+    elif ball.y - ball.raduis <= 0:
+        ball.y_velocity *= -1
+
+    if ball.x_velocity < 0:
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y * left_paddle.height:
+            if ball.x - ball.raduis <= left_paddle.x + left_paddle.width:
+                ball.x_velocity *= -1
+
+                middle_y = left_paddle.y + left_paddle.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height / 2) / ball.MAX_VELOCITY
+                y_vel = difference_in_y / reduction_factor
+                ball.y_velocity = -1 * y_vel
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y * right_paddle.height:
+            if ball.x + ball.raduis >= right_paddle.x:
+                ball.x_velocity *= -1
+
+                middle_y = right_paddle.y + right_paddle.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (right_paddle.height / 2) / ball.MAX_VELOCITY
+                y_vel = difference_in_y / reduction_factor
+                ball.y_velocity = -1 * y_vel
